25efa786e473b68bc54f3d5a183cc7ad68a6e715
Wenderson Carvalho <wendersonmotarauj@gmail.com>
2025-03-25 01:07:29 -0300

    Compactaçao e Download dos Anexos I e II

--- Desafio1/src/Program/ProgramaPrincipal.java
+++ Desafio1/src/Program/ProgramaPrincipal.java
@@ -10,20 +10,20 @@ public class ProgramaPrincipal {
 
     public static void main(String[] args) {
         try {
-            // Instancia o objeto Consultas para obter os links dos PDFs
+          
             Consultas consultas = new Consultas();
             String[] links = consultas.obterLinks();
             
-            // Verifica se o link do primeiro PDF não é nulo e faz o download
+          
             if (links[0] != null) {
                 Downloads.downloadsPdf(links[0], "Anexo_I.pdf");
             }
             
-            // Verifica se o link do segundo PDF não é nulo e faz o download
+          
             if (links[1] != null) {
                 Downloads.downloadsPdf(links[1], "Anexo_II.pdf");
 
-                // Compacta os dois arquivos PDF após o download
+             
                 String[] arquivosParaCompactar = {"anexo" + File.separator + "Anexo_I.pdf", 
                                                    "anexo" + File.separator + "Anexo_II.pdf"};
                 String arquivoZip = "anexo" + File.separator + "anexos.zip";
